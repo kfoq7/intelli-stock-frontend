3e5b92359d4f186588677cb0afacf3e81e7a44b8
kfoq7 <frbrizoi@gmail.com>
2024-05-14 16:41:57 -0500

    feat(features): added select navigation links

--- src/features/core/components/sidebar.tsx
+++ src/features/core/components/sidebar.tsx
@@ -1,17 +1,49 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { Logo } from './logo'
+import { cn } from '@/lib/utils'
+
+const links = [
+  {
+    page: 'Home',
+    to: '/'
+  },
+  {
+    page: 'Inventory',
+    to: '/inventory'
+  },
+  {
+    page: 'Chatbot',
+    to: '/chatbot'
+  }
+]
 
 export function Sidebar() {
+  const pathname = usePathname()
+
   return (
-    <aside className="bg-[#3E447D] w-[290px] lg:w-[310px] h-screen flex flex-col overflow-x-hidden">
-      <div className="grow flex flex-col justify-between">
-        <nav className="text-white flex flex-col">
-          <Link href="/">Inicio</Link>
-          <Link href="/inventory">Inventario</Link>
-          <Link href="/chatbot">Chatbot</Link>
+    <aside className="bg-gradient-to-t from-[#203040] from-70% to-[#283a4d] w-[290px] lg:w-[310px] h-screen flex flex-col overflow-x-hidden">
+      <Logo />
+
+      <div className="grow flex flex-col justify-between p-2 mt-4">
+        <nav className="text-white flex flex-col gap-y-1">
+          {links.map(({ page, to }) => (
+            <Link
+              key={page}
+              href={to}
+              className={cn('py-2 px-3 rounded-md hover:bg-[#18416e]', {
+                'bg-[#214a75] hover:bg-[#214a75]': pathname === to
+              })}
+            >
+              {page}
+            </Link>
+          ))}
         </nav>
 
-        <div className="text-white p-5">
-          <button>Cerrar sesion</button>
+        <div className="text-white">
+          <button className="">Cerrar sesion</button>
         </div>
       </div>
     </aside>
